d5e8e0ad375dcad0036819189f77703ac2aa5073
sscarry2 <earth.narongsak@gmail.com>
2021-11-05 22:51:22 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    return res.send('Hello World')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on 3000');
+    
+})
