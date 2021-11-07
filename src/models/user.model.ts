3aa6514ba758b62fbb33fe2ae29ab3d6bca15368
sscarry2 <earth.narongsak@gmail.com>
2021-11-08 02:26:22 +0700

    update user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -46,9 +46,9 @@ userSchema.pre("save", async function(next){
 userSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean> {
     const user = this as UserDocument
 
-    return bcrypt.compare(candidatePassword, user.password).catch(e=> false)
+    return bcrypt.compare(candidatePassword, user.password).catch(e => false)
 }
 
-const UserModel = mongoose.model("User", userSchema)
+const UserModel = mongoose.model<UserDocument>("User", userSchema)
 
 export default UserModel
