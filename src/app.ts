0d5e52b05b0bbecc9b84fec2816e2fdb417781c7
sscarry2 <earth.narongsak@gmail.com>
2021-11-08 13:20:46 +0700

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import config from 'config'
 import connect from './utils/connect'
 import logger from './utils/logger'
 import routes from './routes'
+import deserializeUser from './middleware/deserializeUser'
 
 const port = config.get<string>('port')
 
@@ -11,6 +12,8 @@ const port = config.get<string>('port')
 const app = express()
 
 app.use(express.json())
+
+app.use(deserializeUser)
 
 app.get('/', (req, res) => {
     return res.send('Hello World')
