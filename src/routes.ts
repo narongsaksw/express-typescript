e78e80f5fdc20d8e0a342f56202dba1fb0e3675e
sscarry2 <earth.narongsak@gmail.com>
2021-11-08 13:20:25 +0700

    update routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Express, Request, Response } from 'express'
-import { createUserSessionHandler } from './controllers/session.controller'
+import { createUserSessionHandler, getUserSessionHandler } from './controllers/session.controller'
 import { createUserHandler } from './controllers/user.controller'
+import requireUser from './middleware/requireUser'
 import validateResource from './middleware/validateResource'
 import { createSessionSchema } from './schema/session.schema'
 import { createUserSchema } from './schema/user.schema'
@@ -12,6 +13,7 @@ function routes(app: Express) {
 
     app.post('/api/users', validateResource(createUserSchema),createUserHandler)
     app.post('/api/sessions', validateResource(createSessionSchema),createUserSessionHandler)
+    app.get('/api/sessions', requireUser, getUserSessionHandler)
 }
 
 export default routes
